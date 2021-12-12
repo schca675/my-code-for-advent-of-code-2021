a230cbb427c7b2f11013ee791b0b182d974f5329
s1830929 <c.schmit@student.utwente.nl>
2021-12-12 17:34:13 +0100

    Add solution to part 2

--- Day12/12_passage_pathing.py
+++ Day12/12_passage_pathing.py
@@ -1,8 +1,34 @@
 # --- Day 12: Passage Pathing ---
 import copy
 
-
 class Path:
+    def __init__(self, cave, steps_taken, next_step, has_visited_lower_caves, lower_case_visited_twice):
+        self.lower_cave_visited_twice = lower_case_visited_twice
+        self.has_visited_lower_caves = has_visited_lower_caves
+        if len(has_visited_lower_caves) == 0:
+            self.lower_cave_visited_twice = False
+            for node in cave.nodes:
+                # set all nodes to False, upper-case ones will stay false
+                self.has_visited_lower_caves[node] = 0
+        # if it is a lower case step: increase number of times it was visited
+        if next_step == next_step.lower():
+            self.has_visited_lower_caves[next_step] += 1
+            if self.has_visited_lower_caves[next_step] == 2:
+                self.lower_cave_visited_twice = True
+        self.steps_taken = steps_taken
+        self.steps_taken.append(next_step)
+
+    def can_steps_be_taken(self, next_step):
+        if next_step != next_step.lower():
+            # upper case steps can always be taken
+            return True
+        if next_step == "start":
+            return False
+        if self.lower_cave_visited_twice:
+            return self.has_visited_lower_caves[next_step] == 0
+        return self.has_visited_lower_caves[next_step] <= 1
+
+class Path_part1:
     def __init__(self, cave, steps_taken, next_step, has_visited_lower_caves):
         self.has_visited_lower_caves = has_visited_lower_caves
         if len(has_visited_lower_caves) == 0:
@@ -41,8 +67,11 @@ class Cave:
                 continue
             # Else: Check next steps and add those to final list
             for poss_steps in self.neighbours[path.steps_taken[-1]]:
-                if not path.has_visited_lower_caves[poss_steps]:
-                    new_path = Path(self, copy.deepcopy(path.steps_taken), poss_steps, copy.deepcopy(path.has_visited_lower_caves))
+                # if not path.has_visited_lower_caves[poss_steps]: ## for part 1
+                if path.can_steps_be_taken(poss_steps):
+                    # new_path = Path(self, copy.deepcopy(path.steps_taken), poss_steps, copy.deepcopy(path.has_visited_lower_caves))
+                    new_path = Path(self, copy.deepcopy(path.steps_taken), poss_steps,
+                                    copy.deepcopy(path.has_visited_lower_caves), copy.deepcopy(path.lower_cave_visited_twice))
                     updated_paths.append(new_path)
             all_paths.extend(self.get_all_paths(updated_paths))
         return all_paths
@@ -60,20 +89,14 @@ def get_puzzle_input(filepath):
     return Cave(nodes, edges)
 
 
-def resolve_puzzle_part1(filepath):
+def resolve_puzzle(filepath):
     cave = get_puzzle_input(filepath)
-    paths = cave.get_all_paths([Path(cave, [], 'start', dict())])
+    paths = cave.get_all_paths([Path(cave, [], 'start', dict(), False)])
     print("PUZZLE SOLUTION: {} unique paths".format(len(paths)))
 
-def resolve_puzzle_part2(filepath):
-    pass
+
 
 print("TEST")
-resolve_puzzle_part1("test_data.txt")
+resolve_puzzle("test_data.txt")
 print("PUZZLE")
-resolve_puzzle_part1("data.txt")
-#
-# print("TEST")
-# resolve_puzzle_part2("test_data.txt")
-# print("PUZZLE")
-# resolve_puzzle_part2("data.txt")
+resolve_puzzle("data.txt")  ## takes a while (<1min) --> not very efficient
