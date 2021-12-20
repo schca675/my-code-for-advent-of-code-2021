b5d22fc8bf566a2229dc33a6d75cf5711af3272b
s1830929 <c.schmit@student.utwente.nl>
2021-12-20 14:42:44 +0100

    Initial commit for day 18 and 19

---
+++ Day18/18_snailfish.py
@@ -0,0 +1,31 @@
+# --- Day 18: Snailfish ---
+
+
+def get_puzzle_input(filepath):
+    field = []
+    image_alg = ""
+    first_line = True
+    with open(filepath) as f:
+        for line in f:
+            if first_line:
+                image_alg = line.rstrip()
+                first_line = False
+                continue
+            if len(line) > 1:
+                # FIeld
+                field.append(line.rstrip())
+    return image_alg, field
+
+def resolve_puzzle(filepath, times):
+    image_alg, field = get_puzzle_input(filepath)
+    new_field_bin, new_field = apply_image_processing(field, image_alg, times)
+    lights = count_light_pixels(new_field_bin)
+    print("{}\nPUZZLE SOLUTION: {} light pixels".format(filepath, lights))
+
+print("Part 1")
+resolve_puzzle("test_data.txt", 2)
+resolve_puzzle("data.txt", 2)
+
+print( "Part 2")
+resolve_puzzle("test_data.txt", 50)
+resolve_puzzle("data.txt", 50)
